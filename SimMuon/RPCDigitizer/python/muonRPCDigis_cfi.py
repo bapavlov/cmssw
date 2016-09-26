d01856789d363f1b7d9c2ffec8377405c0aa29c1
Borislav Pavlov <Borislav.Pavlov@cern.ch>
2016-09-26 19:19:07 +0200

    IRPC Y coordinate

--- SimMuon/RPCDigitizer/python/muonRPCDigis_cfi.py
+++ SimMuon/RPCDigitizer/python/muonRPCDigis_cfi.py
@@ -77,12 +77,8 @@ if eras.phase2_muon.isChosen():
             timeJitter = cms.double(1.0),
             IRPC_time_resolution = cms.double(0.1),
             IRPC_electronics_jitter = cms.double(0.025),
-            N_strips = cms.int32(192),
-#            N_strips = cms.int32(96),
-		    sigmaY = cms.double(0.7), #math.sqrt((0.025)*(0.025)+(0.025)*(0.025))*0.66*299792458*1e+2*1e-9
+	    sigmaY = cms.double(0.7), #math.sqrt((0.025)*(0.025)+(0.025)*(0.025))*0.66*299792458*1e+2*1e-9
             do_Y_coordinate = cms.bool(False)
             ),       
-#                                     digiIRPCModel = cms.string('RPCSimModelTiming')
-                                     digiIRPCModel = cms.string('RPCSimModelPseudodigi')
-
+                                     digiIRPCModel = cms.string('RPCSimModelTiming')
                                      )
